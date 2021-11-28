c3233b57601209d571a207fba2a6311f9e374f7b
Valentin <valentin.riess@posteo.de>
2021-11-28 16:21:36 +0100

    Create classes for abstract moves.

---
+++ src/main/java/com/chess/backend/gamemodel/abstractmoves/MoveOneDiagonal.java
@@ -0,0 +1,25 @@
+package com.chess.backend.gamemodel.abstractmoves;
+
+import com.chess.backend.gamemodel.Game;
+import com.chess.backend.gamemodel.Move;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MoveOneDiagonal {
+
+    public MoveOneDiagonal() {}
+
+    /**
+     * Generate concrete possible moves from a given piece and game context.
+     * @param game Game context
+     * @param attack Allow moves to occupied fields (pawn may not attack straight forward)
+     * @param jump Allow moves that pass occupied fields (knight)
+     * @return HashSet of concrete moves
+     */
+    public static Set<Move> concretise(Game game, boolean attack, boolean jump){
+
+        return new HashSet<>();
+    }
+
+}
