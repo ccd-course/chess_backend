506e259be52dd3bbe3f1bff8f6f1747b32e250c4
Valentin <valentin.riess@posteo.de>
2021-11-28 18:20:33 +0100

    Add two additional abstract moves due to the circular board layout.

---
+++ src/main/java/com/chess/backend/gamemodel/abstractmoves/MoveTwoBackward.java
@@ -0,0 +1,26 @@
+package com.chess.backend.gamemodel.abstractmoves;
+
+import com.chess.backend.gamemodel.Game;
+import com.chess.backend.gamemodel.Move;
+import com.chess.backend.gamemodel.Square;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MoveTwoBackward {
+
+    public MoveTwoBackward() {}
+
+    /**
+     * Generate concrete possible moves from a given piece and game context.
+     * @param game Game context
+     * @param attack Allow moves to occupied fields (pawn may not attack straight forward)
+     * @param jump Allow moves that pass occupied fields (knight)
+     * @return HashSet of concrete moves
+     */
+    public static Set<Move> concretise(Game game, Square square, boolean attack, boolean jump){
+
+        return new HashSet<>();
+    }
+
+}
