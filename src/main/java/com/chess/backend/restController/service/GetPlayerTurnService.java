1e3a059c14b3001f04f7087e4a01df053be9d152
HannesSttzr <hannes.stuetzer@gmail.com>
2021-12-02 20:53:10 +0100

    fix: added missing anotation

--- src/main/java/com/chess/backend/restController/service/GetPlayerTurnService.java
+++ src/main/java/com/chess/backend/restController/service/GetPlayerTurnService.java
@@ -2,7 +2,9 @@ package com.chess.backend.restController.service;
 
 import com.chess.backend.service.GameService;
 import com.chess.backend.restController.objects.GetPlayerTurnObject;
+import org.springframework.stereotype.Service;
 
+@Service
 public class GetPlayerTurnService {
     public GetPlayerTurnObject getPlayerTurn(int gameID){
         GameService gc = GameService.getInstance();
