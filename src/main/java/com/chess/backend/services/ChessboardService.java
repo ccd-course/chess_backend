bed95465f3a664d01a56784790099b3669c6d337
Valentin <valentin.riess@posteo.de>
2021-12-05 13:29:06 +0100

    Make ChessboardService methods static as there is no need to create a instance.

--- src/main/java/com/chess/backend/services/ChessboardService.java
+++ src/main/java/com/chess/backend/services/ChessboardService.java
@@ -1,13 +1,18 @@
 package com.chess.backend.services;
 
 import com.chess.backend.gamemodel.*;
+import com.chess.backend.gamemodel.constants.Color;
 import com.chess.backend.gamemodel.constants.PieceType;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Component
 public class ChessboardService {
 
-    public Chessboard initNewGameBoard(String[] players){
+    public static Chessboard initNewGameBoard(String[] players) {
         Chessboard chessboard = new Chessboard();
         chessboard.setNumberOfPlayers(players.length);
         Square[][] squares = new Square[10][4];
@@ -17,47 +22,44 @@ public class ChessboardService {
                 squares[i][j] = new Square(i,j, null);
             }
         }
-        for(int i=0;i< players.length; i++){
+        for (int i = 0; i < players.length; i++) {
             Player player = new Player(players[i], i);
-            this.initPlayerPieces(squares, player);
+            initPlayerPieces(squares, player);
         }
 
         chessboard.setSquares(squares);
         return chessboard;
     }
 
-    private Square[][] initPlayerPieces(Square[][] squares, Player player) {
+    private static void initPlayerPieces(Square[][] squares, Player player) {
         initPlayerPawns(squares, player);
         initPlayerFigures(squares, player);
-        return squares;
     }
 
-    private Square[][] initPlayerPawns(Square[][] squares, Player player) {
+    private static void initPlayerPawns(Square[][] squares, Player player) {
         int playerFirstColumn = player.getId() * 8;
-        for(int i =0; i< squares.length;i++){
-            this.setPiece(i, playerFirstColumn, squares, new Piece(PieceType.PAWN, player, true));
-            this.setPiece(i, playerFirstColumn + 3, squares, new Piece(PieceType.PAWN, player, false));
+        for (int i = 0; i < squares.length; i++) {
+            setPiece(i, playerFirstColumn, squares, new Piece(PieceType.PAWN, player, true));
+            setPiece(i, playerFirstColumn + 3, squares, new Piece(PieceType.PAWN, player, false));
         }
-        return squares;
     }
-    private Square[][] initPlayerFigures(Square[][] squares, Player player) {
-        int figuresFirstColumn = player.getId() * 8 +1;
 
-        for(int i =0; i< 2;i++){
-            this.setPiece(2, figuresFirstColumn+i, squares, new Piece(PieceType.KNIGHT, player, true));
-            this.setPiece(3, figuresFirstColumn+i, squares, new Piece(PieceType.ROOK, player, true));
+    private static void initPlayerFigures(Square[][] squares, Player player) {
+        int figuresFirstColumn = player.getId() * 8 + 1;
+
+        for (int i = 0; i < 2; i++) {
+            setPiece(2, figuresFirstColumn + i, squares, new Piece(PieceType.KNIGHT, player, true));
+            setPiece(3, figuresFirstColumn + i, squares, new Piece(PieceType.ROOK, player, true));
 //          this.setPiece(i, player.getId() + 3, squares, new Piece(PieceType.PAWN, player));
         }
-        this.setPiece(3, figuresFirstColumn, squares, new Piece(PieceType.KING, player, true));
-        this.setPiece(3, figuresFirstColumn+1, squares, new Piece(PieceType.QUEEN, player, true));
-
-        return squares;
+        setPiece(3, figuresFirstColumn, squares, new Piece(PieceType.KING, player, true));
+        setPiece(3, figuresFirstColumn + 1, squares, new Piece(PieceType.QUEEN, player, true));
     }
-    private Square[][] setPiece(int pozX, int pozY, Square[][] squares, Piece piece){
+
+    private static void setPiece(int pozX, int pozY, Square[][] squares, Piece piece) {
         Square square = squares[pozX][pozY];
         square.setPiece(piece);
         squares[pozX][pozY] = square;
-        return squares;
     }
 
 }
