808db473ea68cb586292af552e97347b693b4634
Valentin <valentin.riess@posteo.de>
2021-12-05 15:05:22 +0100

    Update test

---
+++ src/test/java/com/chess/backend/GameInitializationTests.java
@@ -0,0 +1,28 @@
+package com.chess.backend;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.chess.backend.gamemodel.Game;
+import com.chess.backend.gamemodel.Square;
+import com.chess.backend.gamemodel.constants.Color;
+import com.chess.backend.gamemodel.constants.PieceType;
+import com.chess.backend.services.ChessboardService;
+import com.chess.backend.services.GameService;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class GameInitializationTests {
+
+    @Test
+    void testGameInitialization() {
+        String[] players = {"Test Player 1", "Test Player 2", "Test Player 3"};
+        GameService gameService = new GameService();
+        gameService.createNewGame(players);
+        Game game = gameService.getGame();
+        ArrayList<Square> squares = ChessboardService.searchSquaresByPiece(
+                game.chessboard.squares, PieceType.KING, Color.WHITE, game.getPlayers()[0]);
+
+        assertTrue(squares.size() > 0);
+    }
+
+}
