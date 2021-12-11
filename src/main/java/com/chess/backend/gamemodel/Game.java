ac26a61fa1096e24e3bf50726bd46e438ca116af
Amr Anwar <amranwar945@gmail.com>
2021-12-11 11:51:05 +0100

    refactor: Game

--- src/main/java/com/chess/backend/gamemodel/Game.java
+++ src/main/java/com/chess/backend/gamemodel/Game.java
@@ -1,23 +1,3 @@
-/*
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-/*
- * Authors:
- * Mateusz Sławomir Lach ( matlak, msl )
- * Damian Marciniak
- */
 package com.chess.backend.gamemodel;
 
 import com.chess.backend.services.ChessboardService;
@@ -34,34 +14,12 @@ import java.util.logging.Logger;
  */
 @Data
 public class Game {
-
-    public Settings settings;
     public Chessboard chessboard;
-    public GameClock gameClock;
     public Moves moves;
     private Player activePlayer;
     private int id;
     private Player[] players;
 
-//    public Game()
-//    {
-//        this.moves = new Moves(this);
-//        settings = new Settings();
-//        //TODO: handle the settings, current situation gives an error when creating a game
-//        chessboard = new Chessboard(this.settings, this.moves);
-//        //this.chessboard.
-//        // gameClock = new GameClock(this); // TODO: Implement from old jchess
-//
-//    }
-
-//    /** Method to Start new game
-//     *
-//     */
-//    public void newGame()
-//    {
-//        chessboard.setPieces("", settings.playerWhite, settings.playerBlack);
-//        activePlayer = settings.playerWhite;
-//    }
 
     /**
      * Method to switch active players after move
@@ -91,10 +49,6 @@ public class Game {
 
     public String getActivePlayerName(){
         return this.activePlayer.getName();
-    }
-
-    public int getId() {
-        return this.id;
     }
 
     /**
@@ -137,10 +91,4 @@ public class Game {
         }
     }
 
-    public Player[] getPlayers() {
-        return players;
-    }
-}
-
-class ReadGameError extends Exception {
 }
