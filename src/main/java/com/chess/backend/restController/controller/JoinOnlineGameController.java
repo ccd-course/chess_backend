2fc52037ee43329e5b31b60189d3c1bd5e11e20a
Amr Anwar <amranwar945@gmail.com>
2022-01-27 14:20:45 +0100

    feat: add join controller

--- src/main/java/com/chess/backend/restController/controller/JoinOnlineGameController.java
+++ src/main/java/com/chess/backend/restController/controller/JoinOnlineGameController.java
@@ -1,40 +1,65 @@
-//package com.chess.backend.restController.controller;
-//
-//
-//import com.chess.backend.restController.objects.NewOnlineGameObject;
-//import com.chess.backend.restController.service.NewOnlineGameService;
-//import io.swagger.v3.oas.annotations.Operation;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-///**
-// * This class handles the API-call to create a new game.
-// *
-// * @author Hannes Stuetzer
-// */
-//@RestController
-//@CrossOrigin
-//@RequestMapping("/createOnlineNewGame")
-//public class JoinOnlineGameController {
-//
-//    private final NewOnlineGameService newGameService;
-//
-//    @Autowired
-//    public JoinOnlineGameController(NewOnlineGameService newGameService) {
-//        this.newGameService = newGameService;
-//    }
-//
-//    /**
-//     * Method that is called on a post request.
-//     *
-//     */
-//    @Operation(
-//            summary = "Start a new online game",
-//            description = "Returns the Game ID of a newly created game."
-//    )
-//    @PostMapping
-//    public int joinOnlineGameController(@RequestParam(value = "gameID") int gameID) {
-//          return 1;
-////        return newGameService.getNewGameID(newGameObject);
-//    }
-//}
+package com.chess.backend.restController.controller;
+
+
+import com.chess.backend.gamemodel.ChessGame;
+import com.chess.backend.gamemodel.Player;
+import com.chess.backend.gamemodel.constants.Event;
+import com.chess.backend.repository.GameRepository;
+import com.chess.backend.restController.objects.JoinOnlineGameObject;
+import com.chess.backend.services.ChessGameService;
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class handles the API-call to create a new game.
+ *
+ * @author Hannes Stuetzer
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/joinOnlineNewGame")
+public class JoinOnlineGameController {
+
+    private final ChessGameService gameService;
+    private final GameRepository gameRepository;
+
+    @Autowired
+    public JoinOnlineGameController(ChessGameService newGameService, GameRepository gameRepository) {
+
+        this.gameRepository = gameRepository;
+        this.gameService = newGameService;
+    }
+
+    /**
+     * Method that is called on a post request.
+     *
+     */
+    @Operation(
+            summary = "Start a new online game",
+            description = "Returns the Game ID of a newly created game."
+    )
+    @PostMapping
+    public int joinOnlineGame(@RequestBody JoinOnlineGameObject joinOnlineGameObject) {
+        ChessGame game = this.gameService.getGame(joinOnlineGameObject.getGameId());
+        List<Player> players = game.getPlayers();
+        for(int i =0; i< players.size(); i++){
+            Player player = players.get(i);
+            if(player.getName()==null){
+                player.setName(joinOnlineGameObject.getPlayer());
+                players.set(i, player);
+            }
+        }
+        game.setPlayers(players);
+        game.setEvents(new ArrayList<>());
+        List<Event> events = game.getEvents();
+
+        events.add(Event.NEW_PLAYER_JOIN);
+        game.setEvents(events);
+        this.gameRepository.createNewGame(game.getId(), game);
+        return game.getId();
+    }
+}
