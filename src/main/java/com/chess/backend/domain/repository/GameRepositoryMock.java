852424ed85355dc842c0c59b75b712f179524296
Valentin <valentin.riess@posteo.de>
2022-01-28 13:33:35 +0100

    Create GameRepository Mock

---
+++ src/main/java/com/chess/backend/domain/repository/GameRepositoryMock.java
@@ -0,0 +1,20 @@
+package com.chess.backend.domain.repository;
+
+import com.chess.backend.gamemodel.ChessGame;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class GameRepositoryMock implements IGameRepository {
+    private HashMap<Integer, ChessGame> games = new HashMap<>();
+
+    @Override
+    public ChessGame getGame(Integer gameId) {
+        games.get(gameId);
+    }
+
+    @Override
+    public void createNewGame(Integer gameId, ChessGame game) {
+        games.put(gameId, game);
+    }
+}
